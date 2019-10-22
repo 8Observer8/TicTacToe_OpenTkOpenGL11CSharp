30538ee0802a169ecf2532fe5378a13fee124e11
Ivan Enzhaev <8observer8@gmail.com>
2019-10-22 15:53:46 +0400

    First commit

---
+++ TicTacToe/Window.cs
@@ -0,0 +1,50 @@
+﻿
+using System;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+
+namespace TicTacToe
+{
+    class Window : GameWindow
+    {
+        // Called only once at the beginning
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            Title = "Tic Tac Toe";
+            Width = 256;
+            Height = 256;
+        }
+
+        // Called 60 time per second before OnRenderFrame()
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            base.OnUpdateFrame(e);
+        }
+
+        // Called 60 time per second after OnUpdateFrame()
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
+
+            // Set a viewport
+            GL.Viewport(0, 0, Width, Height);
+
+            // Clears the render canvas
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            // Set a triangle color
+            GL.Color3(0f, 1f, 0f);
+
+            GL.Begin(PrimitiveType.Triangles);
+            GL.Vertex2(-0.5f, -0.5f);
+            GL.Vertex2(0.5f, -0.5f);
+            GL.Vertex2(0f, 0.5f);
+            GL.End();
+
+            // Swaps the front and back buffer
+            SwapBuffers();
+        }
+    }
+}
