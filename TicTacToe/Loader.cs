606a910cec79af9c8d2e5b40e8056d93771d12a7
Ivan Enzhaev <8observer8@gmail.com>
2019-10-23 13:12:39 +0400

    Load a texture and draw one item from the loaded texture

---
+++ TicTacToe/Loader.cs
@@ -0,0 +1,51 @@
+﻿
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using OpenTK.Graphics.OpenGL;
+
+namespace TicTacToe
+{
+    class Loader
+    {
+        public static int LoadTexture(string fileName)
+        {
+            // Create an image object
+            Bitmap image = null;
+            try
+            {
+                image = new Bitmap(fileName);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Failed to load the image: " + fileName);
+                return -1;
+            }
+
+            // Enable unit0
+            GL.ActiveTexture(TextureUnit.Texture0);
+
+            // Create a texture object
+            int texture = GL.GenTexture();
+
+            // Bind the texture object to the target
+            GL.BindTexture(TextureTarget.Texture2D, texture);
+
+            // Set the texture parameters
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)All.Linear);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapS, (int)All.ClampToEdge);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapT, (int)All.ClampToEdge);
+
+            BitmapData data = image.LockBits(
+                new Rectangle(0, 0, image.Width, image.Height),
+                ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format32bppRgb);
+
+            // Set the texture image
+            GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgb, image.Width, image.Height, 0, OpenTK.Graphics.OpenGL.PixelFormat.Bgra, PixelType.UnsignedByte, data.Scan0);
+
+            image.UnlockBits(data);
+
+            return texture;
+        }
+    }
+}
--- TicTacToe/Window.cs
+++ TicTacToe/Window.cs
@@ -14,6 +14,8 @@ namespace TicTacToe
             { '0', ' ', ' ' }
         };
 
+        private bool _canDraw = false;
+
         // Called only once at the beginning
         protected override void OnLoad(EventArgs e)
         {
@@ -22,6 +24,21 @@ namespace TicTacToe
             Title = "Tic-Tac-Toe";
             Width = 192;
             Height = 192;
+
+            // Enable textures and transparentcy
+            GL.Enable(EnableCap.Texture2D);
+            GL.Enable(EnableCap.Blend);
+            GL.BlendFunc(BlendingFactor.SrcAlpha, BlendingFactor.OneMinusSrcAlpha);
+
+            Setup2DGraphics(Width, Height);
+
+            int texture = Loader.LoadTexture("Assets/Textures/GameField.png");
+            if (texture == -1)
+            {
+                return;
+            }
+
+            _canDraw = true;
         }
 
         // Called 60 time per second before OnRenderFrame()
@@ -41,17 +58,35 @@ namespace TicTacToe
             // Clears the render canvas
             GL.Clear(ClearBufferMask.ColorBufferBit);
 
+            if (!_canDraw) return;
+
             // Set a triangle color
-            GL.Color3(0f, 1f, 0f);
+            //GL.Color3(0f, 1f, 0f);
 
             GL.Begin(PrimitiveType.Triangles);
-            GL.Vertex2(-0.5f, -0.5f);
-            GL.Vertex2(0.5f, -0.5f);
-            GL.Vertex2(0f, 0.5f);
+            {
+                // Left top triangle
+                GL.TexCoord2(0f, 0f); GL.Vertex2(0f, 0f);
+                GL.TexCoord2(0f, 0.33f); GL.Vertex2(0f, 64f);
+                GL.TexCoord2(0.33f, 0f); GL.Vertex2(64f, 0f);
+                // Right bottom triangle
+                GL.TexCoord2(0f, 0.33f); GL.Vertex2(0f, 64f);
+                GL.TexCoord2(0.33f, 0.33f); GL.Vertex2(64f, 64f);
+                GL.TexCoord2(0.33f, 0f); GL.Vertex2(64f, 0f);
+            }
             GL.End();
 
             // Swaps the front and back buffer
             SwapBuffers();
+        }
+
+        private void Setup2DGraphics(float width, float height)
+        {
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadIdentity();
+            GL.Ortho(0f, width, height, 0f, -100f, 100f);
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadIdentity();
         }
     }
 }
